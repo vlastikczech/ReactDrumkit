9e7f332900d230bc9c65dc2366b67edaa38e0725
vlastik <vlastikczech1@gmail.com>
2018-10-26 19:35:21 -0700

    Updated the drumkit code completely so that it plays properly

---
+++ src/Drumkit.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+import Drumpads from './Drumpads'
+
+export default class Drumkit extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            display: 'Click or Press a Key'
+        }
+    }
+
+    handleDisplay = display => this.setState({ display })
+
+    render(){
+        return(
+            <div id='drum-machine'>
+                <div id='display'>{this.state.display}</div>
+                <div id='drum-pads'>{boardOne.map(d => (
+                    <Drumpads
+                        key={d.id}
+                        id={d.id}
+                        keycode={d.keyCode}
+                        letter={d.keyTrigger}
+                        src={d.src}
+                        handleDisplay={this.handleDisplay}
+                    />
+                ))}</div>
+            </div>
+        )
+    }
+}
+const boardOne = [{
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+}, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+}, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+}, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+}, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+}, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+}, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+},];
